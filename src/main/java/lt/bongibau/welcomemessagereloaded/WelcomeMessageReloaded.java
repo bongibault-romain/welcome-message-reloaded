fe2a9e6ac5e3edb18fa3dfa4af666645ff4efa7b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-28 20:09:53 +0200

    initial commit

---
+++ src/main/java/lt/bongibau/welcomemessagereloaded/WelcomeMessageReloaded.java
@@ -0,0 +1,118 @@
+package lt.bongibau.welcomemessagereloaded;
+
+import lt.bongibau.welcomemessagereloaded.configuration.LYamlConfigurationFile;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public final class WelcomeMessageReloaded extends JavaPlugin implements Listener, CommandExecutor {
+
+    private WelcomeMessageReloaded instance = null;
+
+    private final LYamlConfigurationFile config = new LYamlConfigurationFile("config", this);
+
+    private final HashMap<String, String> replacements = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        this.config.load();
+
+        this.getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(this.getCommand("wmr")).setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    public void register(String key, String value) {
+        this.replacements.put(key, value);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0) {
+            sender.sendMessage(this.config.getMessage("messages.command-not-found"));
+            return true;
+        }
+
+        if (sender.hasPermission("wmr.reload")) {
+            this.config.load();
+            sender.sendMessage(this.config.getMessage("messages.config-reloaded"));
+        } else {
+            sender.sendMessage(this.config.getMessage("messages.no-permission"));
+        }
+
+        return true;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        //if (player.hasPlayedBefore()) return;
+
+        this.register("player", player.getName());
+        this.register("online", this.formatNumber(this.getServer().getOnlinePlayers().size()));
+        this.register("maxplayers", this.formatNumber(this.getServer().getMaxPlayers()));
+        this.register("version", this.getServer().getVersion());
+
+        if (this.config.getConfiguration().getBoolean("welcome-message.enabled")) {
+            List<String> messages = this.config.getMessageList("welcome-message.messages", this.replacements);
+
+            for (String message : messages) {
+                player.sendMessage(message);
+            }
+        }
+
+        if (this.config.getConfiguration().getBoolean("welcome-broadcast.enabled")) {
+            List<String> messages = this.config.getMessageList("welcome-broadcast.messages", this.replacements);
+
+            for (String message : messages) {
+                this.getServer().broadcastMessage(message);
+            }
+        }
+
+        if (this.config.getConfiguration().getBoolean("welcome-title.enabled")) {
+            String title = this.config.getMessage("welcome-title.title", this.replacements);
+            String subtitle = this.config.getMessage("welcome-title.subtitle", this.replacements);
+            int fadeIn = this.config.getConfiguration().getInt("welcome-title.fade-in");
+            int stay = this.config.getConfiguration().getInt("welcome-title.stay");
+            int fadeOut = this.config.getConfiguration().getInt("welcome-title.fade-out");
+
+            player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
+        }
+
+        if (this.config.getConfiguration().getBoolean("welcome-sound.enabled")) {
+            String soundString = this.config.getConfiguration().getString("welcome-sound.sound");
+            Sound sound = Sound.valueOf(soundString);
+
+            float volume = this.config.getConfiguration().getInt("welcome-sound.volume");
+            float pitch = this.config.getConfiguration().getInt("welcome-sound.pitch");
+
+            player.playSound(player.getLocation(), sound, volume, pitch);
+        }
+    }
+
+    private String formatNumber(int number) {
+        return new DecimalFormat("#,###,###,###").format(number);
+    }
+
+    public WelcomeMessageReloaded getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/lt/bongibau/welcomemessagereloaded/configuration/LYamlConfigurationFile.java
@@ -0,0 +1,109 @@
+package lt.bongibau.welcomemessagereloaded.configuration;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LYamlConfigurationFile {
+
+    private final String fileName;
+    
+    private final JavaPlugin plugin;
+
+    private YamlConfiguration configuration;
+
+    private File file;
+
+    public LYamlConfigurationFile(String fileName, JavaPlugin plugin) {
+        this.fileName = fileName;
+        this.plugin = plugin;
+        this.configuration = null;
+        this.file = null;
+
+        this.saveDefault();
+    }
+
+    public void load() {
+        this.file = new File(this.plugin.getDataFolder(), this.fileName + ".yml");
+        this.configuration = YamlConfiguration.loadConfiguration(this.file);
+
+        InputStream defaultStream = this.plugin.getResource(this.fileName + ".yml");
+
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.configuration.setDefaults(defaultConfig);
+        }
+    }
+
+    public YamlConfiguration getConfiguration() {
+        if (this.configuration == null) {
+            this.load();
+        }
+
+        return this.configuration;
+    }
+
+    public List<String> getMessageList(String path) {
+        return this.getMessageList(path, new HashMap<>());
+    }
+
+    public List<String> getMessageList(String path, HashMap<String, String> args) {
+        List<String> messages = this.getConfiguration().getStringList(path);
+
+        if (messages == null) return new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : args.entrySet()) {
+            messages.replaceAll(s -> s.replace("%" + entry.getKey().toLowerCase() + "%", entry.getValue()));
+        }
+
+        messages.replaceAll(s -> s.replace("&", "§"));
+        return messages;
+    }
+
+    public String getMessage(String path) {
+        return this.getMessage(path, new HashMap<>());
+    }
+
+    public String getMessage(String path, HashMap<String, String> args) {
+        String message = this.getConfiguration().getString(path);
+
+        if (message == null) return path;
+
+        for (Map.Entry<String, String> entry : args.entrySet()) {
+            message = message.replace("%" + entry.getKey().toLowerCase() + "%", entry.getValue());
+        }
+
+        message = message.replace("&", "§");
+        return message;
+    }
+
+    public void save() {
+        if (this.configuration == null || this.file == null) {
+            return;
+        }
+
+        try {
+            this.getConfiguration().save(this.file);
+        } catch (Exception e) {
+            this.plugin.getLogger().severe("Could not save configuration file " + this.file.getName() + "!");
+        }
+    }
+
+    public void saveDefault() {
+        if (this.file == null) {
+            this.file = new File(this.plugin.getDataFolder(), this.fileName + ".yml");
+        }
+
+        if (!this.file.exists()) {
+            this.plugin.saveResource(this.fileName + ".yml", false);
+        }
+    }
+}
